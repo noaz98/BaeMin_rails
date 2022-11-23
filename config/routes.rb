6b4597df91cc9c5b65f85cfb8a6614e3edc1955c
noaz98 <alsgh0717@gmail.com>
2022-11-23 06:37:26 +0000

    route success

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :homes
   resources :ceos
   resources :reviews
@@ -12,6 +13,14 @@ Rails.application.routes.draw do
   resources :stores
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 	root 'homes#index'
-	# get '/homes/index', to: 'customers#index', as: "mypage"
-	get '/homes/index/:id', to: 'customers#index'
+	
+	get '/mypage' ,to: 'customers#index'
+	get '/dibs', to: 'dibs#index'
+	get '/review', to: 'reviews#index'
+	get '/chicken', to: 'stores#index'
+	
+	get '/user_order', to: 'user_orders#index'
+	get '/store', to: 'menus#index'
+	get '/menu', to: 'baskets#index'
+	get '/order', to: 'orders#index'
 end
